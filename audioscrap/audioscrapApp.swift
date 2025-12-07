626effa4a0f3c7a675fc39b21fb2a45a4ec08268
Thomas Boom <thomas.boom@icloud.com>
2025-12-07 23:55:39 +0100

    Initial Commit

---
+++ audioscrap/audioscrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioscrapApp.swift
+//  audioscrap
+//
+//  Created by Thomas Boom on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct audioscrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
